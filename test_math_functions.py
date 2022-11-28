eb2289e5fd7698c13a37ea07d99d499be4602514
Michael A. Melei <meleim@eos03.cis.gvsu.edu>
2022-11-28 13:44:54 -0500

    fixed test case

--- test_math_functions.py
+++ test_math_functions.py
@@ -14,7 +14,7 @@ def test_calc_multiply():
 
 def test_calc_multiply_fail():
     output = multiply_numbers(2,4)
-    assert output == 16
+    assert output == 8
 
 def test_calc_divide():
     output = divide_numbers(10,2)
